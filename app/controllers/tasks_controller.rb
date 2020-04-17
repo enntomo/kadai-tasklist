dae291f86ed606707dace51f7fade7d4f360c9e7
tomoya <246tomoyne@gmail.com>
2020-04-17 03:37:19 +0000

    user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 before_action :require_user_logged_in
-before_action :correct_user, only: [:destroy, :show, :update,]
+before_action :correct_user, only: [:destroy, :show, :update, :edit]
   def index
       @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(25)
   end
